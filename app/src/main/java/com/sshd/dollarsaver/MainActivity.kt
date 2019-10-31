0392bf7a3e2c074931a083d7a21380959551c704
Siva <siva.teja.st@gmail.com>
2019-10-31 01:19:20 -0400

    code changes

---
+++ app/src/main/java/com/sshd/dollarsaver/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sshd.dollarsaver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+    }
+}
