275a72395a18115efa3b95d11ad0c47a2d4a0ea6
_isrinukollipoti <srinukollipoti@gmail.com>
2019-11-13 15:01:01 -0500

    Validation of login and registration

--- app/src/main/java/com/sshd/dollarsaver/Activities/Login.kt
+++ app/src/main/java/com/sshd/dollarsaver/Activities/Login.kt
@@ -6,20 +6,38 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.sshd.dollarsaver.MainPage
 import com.sshd.dollarsaver.R
-
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+import kotlinx.android.synthetic.main.login_page.*
 class Login : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_page)
+        auth = FirebaseAuth.getInstance()
         //login button listner
         val login_button = findViewById<View>(R.id.login)
         login_button.setOnClickListener {
             //start data base code
+            auth.signInWithEmailAndPassword(username.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(this, "Login succesful", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainPage::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(baseContext, "Login failed", Toast.LENGTH_SHORT).show()
 
-            //get 200OK response
-            val intent = Intent(this, MainPage::class.java)
-            startActivity(intent)
+
+                        //get 200OK response
+
+                    }
+
+                }
         }
 
         //register button listner
@@ -28,6 +46,5 @@ class Login : AppCompatActivity() {
             val intent = Intent(this, RegisterPage::class.java)
             startActivity(intent)
         }
-
     }
 }
