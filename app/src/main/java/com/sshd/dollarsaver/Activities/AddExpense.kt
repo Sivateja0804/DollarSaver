4f8559e2d0f3b44622632cd1cacb9a9b3f0073ac
Siva <siva.teja.st@gmail.com>
2019-11-13 15:43:38 -0500

    removed comma in Json Added 2 custom spinners in addExpense.kt Added a spinner code in add_expense.xml edited the category_list spinner and submit and add expense button height decreased
    Created spinner_item xml
    added market_list in strings.xm

--- app/src/main/java/com/sshd/dollarsaver/Activities/AddExpense.kt
+++ app/src/main/java/com/sshd/dollarsaver/Activities/AddExpense.kt
@@ -1,6 +1,10 @@
 package com.sshd.dollarsaver.Activities
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.sshd.dollarsaver.*
 
@@ -9,6 +13,42 @@ class AddExpense : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_expense)
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val market_spinner=findViewById<Spinner>(R.id.market_spinner)
+        //category spinner
+        ArrayAdapter.createFromResource(this, R.array.category_list, R.layout.spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
 
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Code to perform some action when nothing is selected
+            }
+        }
+
+        //market_spinner
+        ArrayAdapter.createFromResource(this, R.array.market_list, R.layout.spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            market_spinner.adapter = adapter
+        }
+        market_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Code to perform some action when nothing is selected
+            }
+        }
     }
 }
--- app/src/main/java/com/sshd/dollarsaver/Activities/Login.kt
+++ app/src/main/java/com/sshd/dollarsaver/Activities/Login.kt
@@ -32,11 +32,7 @@ class Login : AppCompatActivity() {
                         // If sign in fails, display a message to the user.
                         Toast.makeText(baseContext, "Login failed", Toast.LENGTH_SHORT).show()
 
-
-                        //get 200OK response
-
                     }
-
                 }
         }
 
